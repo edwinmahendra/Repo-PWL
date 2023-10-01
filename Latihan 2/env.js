9f01a5c5314701a2b95378909b0795ab9a2ea67d
Edwin Mahendra <edwin.mahendra@ti.ukdw.ac.id>
2023-10-01 21:51:43 +0700

    Add Tugas Kecil 2

---
+++ Latihan 2/env.js
@@ -0,0 +1,13 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "live_sFLfjTf9bPZpzF3h9J814kmccrJ5thVzb7YAeb3CZYAeQK9qcuABM51onXR8z9eU",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_sFLfjTf9bPZpzF3h9J814kmccrJ5thVzb7YAeb3CZYAeQK9qcuABM51onXR8z9eU",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export { dog_env };
